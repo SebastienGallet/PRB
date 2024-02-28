6e32e3d33aa9f097b5a50cd20c01a416269b4d34
Sebastien Gallet <sgallet.dev@gmail.com>
2024-02-28 15:37:52 +0100

    fin prbv1

--- js/guides.js
+++ js/guides.js
@@ -1,21 +1,21 @@
-document.getElementById('button-gdd').addEventListener('click', function() {
-    this.classList.remove('primary')
-    document.getElementById('button-ecu').classList.add('primary')
-    document.getElementById('gdd').style.display = 'block';
-    document.getElementById('ecumap').style.display = 'none';
-  });
-  
-  document.getElementById('button-ecu').addEventListener('click', function() {
-    this.classList.remove('primary')
-    document.getElementById('button-gdd').classList.add('primary')
-    document.getElementById('gdd').style.display = 'none';
-    document.getElementById('ecumap').style.display = 'block';
-  });
-  
-const iframe = document.getElementById('gdd');
-  iframe.addEventListener('load', function() {
-    
-    const iframeContent = iframe.contentWindow.document.body;
-    const iframeHeight = iframeContent.scrollHeight + 'px';
-    iframe.style.height = iframeHeight;
-  });
+document.getElementById("button-gdd").addEventListener("click", function () {
+  this.classList.remove("primary");
+  document.getElementById("button-ecu").classList.add("primary");
+  document.getElementById("gdd").style.display = "block";
+  document.getElementById("ecumap").style.display = "none";
+});
+
+document.getElementById("button-ecu").addEventListener("click", function () {
+  this.classList.remove("primary");
+  document.getElementById("button-gdd").classList.add("primary");
+  document.getElementById("gdd").style.display = "none";
+  document.getElementById("ecumap").style.display = "block";
+  document.getElementById("gdd").style.height = "2000px";
+});
+
+const iframe = document.getElementById("gdd");
+iframe.addEventListener("load", function () {
+  const iframeContent = iframe.contentWindow.document.body;
+  const iframeHeight = iframeContent.scrollHeight + "2000px";
+  iframe.style.height = iframeHeight;
+});
